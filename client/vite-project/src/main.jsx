353a445683d1454e7403f98aa4110a8c3d6d7978
Swati Singh <Swati@email.com>
2026-04-27 20:24:37 +0530

    Tailwind install & setup completed

--- client/vite-project/src/main.jsx
+++ client/vite-project/src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { jsx } from "react/jsx-runtime";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+);
+jsx;
+function tailwind() {
+  return <h1 className="text-blue-500 text-4xl font-bold">Tailwind Working</h1>;
+}
+export default tailwind;
